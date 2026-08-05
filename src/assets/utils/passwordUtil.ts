9f582ae96037fe331be44c1e0382ce33c99b8a09
Nicolas <nicolasbsliell@gmail.com>
2026-08-05 15:52:00 -0300

    feat: criando o passwordUtil

---
+++ src/assets/utils/passwordUtil.ts
@@ -0,0 +1,10 @@
+import bcrypt from "bcryptjs";
+
+export async function hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(12)
+    return await bcrypt.hash(password, salt)
+}
+
+export async function comparePassword(password: string, hashPassword: string): Promise<boolean> {
+    return await bcrypt.compare(password, hashPassword)
+}
