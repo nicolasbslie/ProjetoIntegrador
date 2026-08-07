954ed3b33e46f249043d49a2c1fae3829d6b210f
Nicolas <nicolasbsliell@gmail.com>
2026-08-07 14:11:26 -0300

    fix: adicionando imports

--- src/assets/config/data-source.ts
+++ src/assets/config/data-source.ts
@@ -13,10 +13,10 @@ export const AppDataSource = new DataSource({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
 
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USER,
+    password: process.env.DB_PWD,
 
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
 
     synchronize: true,
     logging: false,
