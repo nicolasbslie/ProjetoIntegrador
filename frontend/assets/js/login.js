090e6888b3ccd0e3dcf3163b63318b2056dbc9ba
NICOLAS BITTENCOURT STUDZINSKI LIELL <03297273038@senacrs.edu.br>
2026-08-13 17:17:15 -0300

    feat: criando função de logar integrado ao backend

--- frontend/assets/js/login.js
+++ frontend/assets/js/login.js
@@ -1,13 +1,32 @@
-  
-    function entrar() {
+async function entrar() {
       const email = document.getElementById('email').value.trim();
       const senha = document.getElementById('senha').value.trim();
 
-      if (!email || !senha) {
-        alert('Preencha todos os campos.');
-        return;
-      }
+      try {
+        const resposta = await fetch('http://localhost:3000/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email,
+            senha
+          })
+        });
 
-      alert('Login realizado com sucesso!');
+        const dados = await resposta.json();
+
+        if(!resposta.ok) {
+          alert(dados.message || 'Erro ao realizar login do usuário')
+          return
+        }
+
+        alert("Login realizado com sucesso! Bem vindo(a).")
+
+      window.location.href='gasto.html'
+      } catch (erro) {
+        console.error(erro);
+        alert('Não foi possível conectar ao servidor')
+      }
     }
  
