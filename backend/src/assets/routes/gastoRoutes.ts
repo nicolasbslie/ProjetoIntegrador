5813a97ea37af55bfb68b2809636053bf7f47325
NICOLAS BITTENCOURT STUDZINSKI LIELL <03297273038@senacrs.edu.br>
2026-08-17 16:45:35 -0300

    delete: removendo comentário e rota inutilizada

--- backend/src/assets/routes/gastoRoutes.ts
+++ backend/src/assets/routes/gastoRoutes.ts
@@ -8,8 +8,6 @@ router.post("/", authMiddleware, GastoController.create);
 
 router.get("/", authMiddleware, GastoController.findAll);
 
-router.get("/me")
-
 router.get("/:id", authMiddleware, GastoController.findById);
 
 router.patch("/:id", authMiddleware, GastoController.update);
