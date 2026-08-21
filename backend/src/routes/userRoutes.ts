ed498e68292f089c304e49043a16df7fca7fdd84
HENRIQUE BILHAN DE SOUZA <06239484008@senacrs.edu.br>
2026-08-21 14:13:10 -0300

    fix:correção UserRoutes

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -6,6 +6,8 @@ const router = Router();
 
 router.get("/", authMiddleware, UserController.findAll);
 
+router.get("/me", authMiddleware, UserController.findMe);
+
 router.get("/:id", authMiddleware, UserController.findById);
 
 router.patch("/:id", authMiddleware, UserController.update);
