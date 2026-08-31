72b187749f915ac5de9b0a5689eeca0394bd36e4
HENRIQUE BILHAN DE SOUZA <06239484008@senacrs.edu.br>
2026-08-31 14:24:37 -0300

    fix: correção no authroutes.ts

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -6,5 +6,7 @@ const router = Router();
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
 router.post("/logout", AuthController.logout);
+router.post("/forgot-password", AuthController.forgotPassword);
+router.post("/reset-password", AuthController.resetPassword);
 
 export default router;
